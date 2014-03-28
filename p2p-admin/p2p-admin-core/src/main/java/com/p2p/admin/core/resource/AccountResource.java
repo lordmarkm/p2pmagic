739af50facff6ff31e4500f08db86b1c917ca72a
mbmartinez <mbmartinez@exist.com>
2014-03-28 15:02:31 +0800

    swagger works

--- p2p-admin/p2p-admin-core/src/main/java/com/p2p/admin/core/resource/AccountResource.java
+++ p2p-admin/p2p-admin-core/src/main/java/com/p2p/admin/core/resource/AccountResource.java
@@ -5,27 +5,44 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baldy.commons.security.models.Account;
-import com.baldy.commons.security.services.AccountService;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 
 @Api(value = "", description = "Account operations")
 @RequestMapping("/accounts")
 @RestController
 public class AccountResource {
 
-    @Autowired
-    private AccountService accountService;
-    
-    @RequestMapping(method = GET)
-    @ApiOperation("Get all accounts")
-    public ResponseEntity<List<Account>> findAll() {
-        return new ResponseEntity<>(accountService.findAll(), OK);
+    private static Logger log = LoggerFactory.getLogger(AccountResource.class);
+
+    //@Autowired
+    //private AccountService accountService;
+
+    @PostConstruct
+    public void init() {
+        log.debug("Initialized.");
+    }
+
+    @RequestMapping(value = "/test", method = GET)
+    @ApiOperation("Test string return value")
+    public ResponseEntity<Account> test(@ApiParam("name") @RequestParam String name) {
+        log.debug("Test requested.");
+
+        Account account = new Account();
+        account.setPassword("123qwe");
+        account.setUsername(name);
+
+        return new ResponseEntity<>(account, OK);
     }
 }
--- p2p-admin/p2p-admin-http/src/main/java/com/p2p/admin/http/config/AdminHttpConfig.java
+++ p2p-admin/p2p-admin-http/src/main/java/com/p2p/admin/http/config/AdminHttpConfig.java
@@ -4,14 +4,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan(basePackages = {"com.baldy.commons.web.config", "com.p2p.admin.http"})
+@ComponentScan(basePackages = {"com.baldy.commons.web.config", "com.baldy.commons.resourcedoc.config", "com.p2p.admin.http"})
 @PropertySource({"classpath:app.properties"})
 @EnableAspectJAutoProxy
 public class AdminHttpConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+      configurer.enable(); 
+    }
 
 }
